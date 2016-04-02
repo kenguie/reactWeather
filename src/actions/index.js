a809b56844f7af96f294020b909bd15c0a378911
Kenrick Guie <homeImac@Kenricks-iMac.local>
2016-04-02 18:29:34 -0400

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = '45685f2436ca576e1272a4dd6796a257';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;  // ES6 to inject variables into strings = backticks and ${}
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
+
+// http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=&appid=45685f2436ca576e1272a4dd6796a257
