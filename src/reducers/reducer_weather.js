36490232f2b830e8a04fc654ddaa567953bcfba4
Kenrick Guie <homeImac@Kenricks-iMac.local>
2016-04-02 22:25:36 -0400

    break - more es6, props, connect

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
-	console.log('Action received:', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+	switch (action.type) {
+		case FETCH_WEATHER:
+			//return state.concat([action.payload.data]); // adds on to the current state array, creating a new array - never mutate the state!
+			// return a new instance of the state!
+			return [ action.payload.data, ... state ];  
+	}
 	return state;
 }
